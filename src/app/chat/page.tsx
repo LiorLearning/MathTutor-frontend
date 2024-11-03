66cdbb9a747ed4e525f1a452479dd07ae445fb51
Sahasra Ranjan <sahasraranjan@gmail.com>
2024-11-04 00:20:33 +0530

    build fix

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,19 +1,10 @@
-"use client"
-
-import { Chat } from "@/components/chat";
+import Chat from "@/components/chat";
 import React, { Suspense } from 'react';
-import { AudioProvider} from '@/components/utils/audio_stream';
-import { useSearchParams } from 'next/navigation'
 
 export default function ChatPage() {
-  const searchParams = useSearchParams();
-  const username = searchParams.get('username') || 'testuser';
-  
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <AudioProvider clientId={username}>
-        <Chat username={username}/>
-      </AudioProvider>
+      <Chat />
     </Suspense>
   );
 }
